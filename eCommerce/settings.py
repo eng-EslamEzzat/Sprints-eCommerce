635073446a845663156beb3f6bf897ad49710894
eng-EslamEzzat <eslamezat367@gmial.com>
2022-05-30 23:46:27 +0200

    fix: heroku importace

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -68,6 +68,10 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000"
 ]
 
 ROOT_URLCONF = 'eCommerce.urls'
