77455bd8a21fdc447f11691e0fa130e972a7c556
eng-EslamEzzat <eslamezat367@gmial.com>
2022-05-31 16:11:38 +0200

    fix: again

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: python manage.py migrate && gunicorn eCommerce.wsgi
-web: gunicorn eCommerce.wsgi
+web: gunicorn eCommerce.wsgi --log-file -
--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
-import django_on_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,7 +98,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'eCommerce.wsgi.application'
 
-#eslam
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -151,9 +151,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticsfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
