a10e7ed022b7706e6a75d8b50f1a8a6b62a32fb1
eng-EslamEzzat <eslamezat367@gmial.com>
2022-05-31 18:29:52 +0200

    fix: heroku management

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -14,7 +14,6 @@ import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,12 +150,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticsfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import django_heroku
 django_heroku.settings(locals())
